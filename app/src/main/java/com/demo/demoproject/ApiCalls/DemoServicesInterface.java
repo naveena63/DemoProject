8f34f18c145a591d8362ee9dad707f26fb17c135
naveenamoka <naveena@vume.in>
2022-12-10 22:04:30 +0530

    Project setup is done

---
+++ app/src/main/java/com/demo/demoproject/ApiCalls/DemoServicesInterface.java
@@ -0,0 +1,20 @@
+package com.demo.demoproject.ApiCalls;
+
+import com.demo.demoproject.model.ListItemsResponseDataItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface DemoServicesInterface {
+
+
+
+    @GET("/v2/list?")
+     Call<List<ListItemsResponseDataItem>> getListItems();
+
+    // ToDo Change ResponseBody
+}
