fcbd01cd937baa8c0f83bd60821f8d2d9407d569
naveenamoka <naveena@vume.in>
2022-12-10 17:05:56 +0530

    Demo Project

---
+++ app/src/main/java/com/demo/demoproject/SplashSceenActivity.java
@@ -0,0 +1,14 @@
+package com.demo.demoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashSceenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
