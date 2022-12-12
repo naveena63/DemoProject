d7e5ce0a16acf4a5de12ef31275f547c8388bf81
naveenamoka <naveena@vume.in>
2022-12-12 06:39:02 +0530

    Added refresh button in ListViewActivity

--- app/src/main/java/com/demo/demoproject/ListViewActivity.java
+++ app/src/main/java/com/demo/demoproject/ListViewActivity.java
@@ -1,11 +1,14 @@
 package com.demo.demoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.demo.demoproject.Adapter.ListItemadapter;
 import com.demo.demoproject.ApiCalls.DemoServicesInterface;
@@ -22,22 +25,33 @@ import retrofit2.Response;
 public class ListViewActivity extends AppCompatActivity {
     RecyclerView cardRecyclerview;
     List<ListItemsResponseDataItem> listItemsResponseDataItems;
-
+ImageView refreshIV;
     ListItemadapter listItemadapter;
+    ContentLoadingProgressBar loading_bar;
     public static final String LIMIT = "20";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         cardRecyclerview = findViewById(R.id.card_recyclerview);
+        refreshIV=findViewById(R.id.refresh_button);
+        loading_bar=findViewById(R.id.loading_bar);
         listItemsResponseDataItems = new ArrayList<>();
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         cardRecyclerview.setLayoutManager(layoutManager);
         listItemadapter = new ListItemadapter(getApplicationContext(), listItemsResponseDataItems);
         cardRecyclerview.setAdapter(listItemadapter);
         loadData(2);
-
+refreshIV.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        listItemadapter.notifyDataSetChanged();
+        loadData(2);
+        loading_bar.setVisibility(View.VISIBLE);
     }
+});
+    }
+
     private void loadData(int page)
     {
 
@@ -57,4 +71,5 @@ public class ListViewActivity extends AppCompatActivity {
             }
         });
     }
+
 }
